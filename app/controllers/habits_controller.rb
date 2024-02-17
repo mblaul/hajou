bfb2266a82a338378419fb7d83ec9e3ecede630a
Matt Blaul <mattblaul@gmail.com>
2024-02-17 14:13:35 -0500

    Working on habit routes

--- app/controllers/habits_controller.rb
+++ app/controllers/habits_controller.rb
@@ -7,7 +7,23 @@ class HabitsController < ApplicationController
     @habit = Habit.find(params[:id])
   end
 
-  def create
+  def new
+    @habit = Habit.new
+  end
 
+  def create
+    @habit = Habit.new(habit_params)
+
+    if @habit.save
+      redirect_to @habit
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def habit_params
+    params.require(:habit).permit(:name, :description)
   end
 end
