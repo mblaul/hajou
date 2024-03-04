319dba34772d5e12418f9d9e103d692e8aee0cae
Matt Blaul <mattblaul@gmail.com>
2024-03-03 23:52:52 -0500

    Light style updates for habit entries

---
+++ app/helpers/habit_entry_ratings_helper.rb
@@ -0,0 +1,13 @@
+module HabitEntryRatingsHelper
+  def habit_entry_rating_to_icon(habit_entry_rating_value)
+     # vs code yells at me no matter how I store these emojis...
+    icons = {
+      1 => '🤬',
+      2 => '😠',
+      3 => '😌',
+      4 => '😀',
+      5 => '😎'
+    }
+    icons[habit_entry_rating_value]
+  end
+end
